8bf52e0ebdbacf624e865ba8f460008e09319ddd
Orzemes Mirzov <mr.mirzov@yandex.ru>
2020-06-13 18:19:57 +0300

    Add files via upload

---
+++ precache-manifest.f83528148eb02f46c7a7706233e16395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a442fbc9159be6a4e35d3fbca6a76d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2abcec1392b4cdeeca9",
+    "url": "/static/css/main.e9a36b84.chunk.css"
+  },
+  {
+    "revision": "ac0dbd0197876cccea20",
+    "url": "/static/js/2.71ccaa24.chunk.js"
+  },
+  {
+    "revision": "9233ae6362c2d4da784147a21eceacce",
+    "url": "/static/js/2.71ccaa24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2abcec1392b4cdeeca9",
+    "url": "/static/js/main.989b3c4c.chunk.js"
+  },
+  {
+    "revision": "580cd99e3adc46fc0350",
+    "url": "/static/js/runtime-main.7ecd5199.js"
+  }
+]);
